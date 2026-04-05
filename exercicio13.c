a3f6f86b3c471200677f69fe57825376dc45cbcd
Emanuelle Santos <221558432+ElleYstar@users.noreply.github.com>
2026-04-04 22:03:19 -0300

    Add files via upload

---
+++ exercicio13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int C, Z;
+    scanf("%d", &C);
+
+    Z = 3 * C - 15;
+
+    printf("%d graus Celsius equivalem a %d graus Zork\n", C, Z);
+
+    return 0;
+}
